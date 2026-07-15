e5e0849a4ad6bb9cce7569525a3d5c82f79b70a2
Ramesh Aravindh <ramesh.aravindha@digit7.ai>
2026-07-15 11:00:01 +0530

    Add data_types.py with examples of variable types and type conversions

---
+++ data_types.py
@@ -0,0 +1,106 @@
+# # String
+# name = "Ramesh"
+# company = "Digit7"
+
+# print("Employee Name:", name)
+# print("Company:", company)
+
+# # Integer
+
+# age = 28
+# test_cases = 512   
+
+# print("Age:", age)
+# print("Test Cases:", test_cases)
+
+# # Float
+# salary = 50000.75
+# completion_rate = 98.56
+
+# print("Salary:", salary)
+# print("Completion Rate:", completion_rate)
+
+# # Boolean
+# is_loggedLin = True
+# is_admin = False    
+
+# print("Is Logged In:", is_loggedLin)
+# print("Is Admin:", is_admin)
+
+
+# # Check the data types
+# name = "Ramesh"
+# age = 28
+# salary = 50000.50
+# is_employee = True
+
+# print("Type of name:", type(name))
+# print("Type of age:", type(age))
+# print("Type of salary:", type(salary))
+# print("Type of is_employee:", type(is_employee))
+
+# # Type Conversion String to Integer
+# age = "28"
+
+# print("=========================Conversion String to Integer=========================")
+# print(f"Age is:, {age} and its type is: {type(age)}")
+
+# age = int(age)
+# print(f"Age is:, {age} and its converted type is: {type(age)}")
+
+# # Convert Integer → String
+# count = 100
+# print("=========================Conversion Integer to String=========================")
+# print(f"Count is:, {count} and its type is: {type(count)}")
+# count = str(count)
+# print(f"Count is:, {count} and its converted type is: {type(count)}")
+
+# #Convert Integer → Float
+# marks = 95
+# print("=========================Conversion Integer to Float=========================")
+# print(f"Marks is:, {marks} and its type is: {type(marks)}")
+# marks = float(marks)
+# print(f"Marks is:, {marks} and its converted type is: {type(marks)}")
+
+# #Convert Float → Integer
+# price = 250.89
+# print("=========================Conversion Float to Integer=========================")
+# print(f"Price is:, {price} and its type is: {type(price)}")
+# price = int(price)
+# print(f"Price is:, {price} and its converted type is: {type(price)}")
+
+# #Example of Type Conversion String to Integer
+
+# total_transactions = "141"
+# print("=========================Conversion String to Integer=========================")
+# print(f"Total Transactions is:, {total_transactions} and its type is: {type(total_transactions)}")
+
+# total_transactions = int(total_transactions)
+# print(f"Total Transactions is:, {total_transactions} and its converted type is: {type(total_transactions)}")
+# print(f"After adding 10 to total_transactions, the result is: {total_transactions + 10}")
+# print(f"Now the type of total_transactions is: {type(total_transactions)}")
+
+
+# print("Exercise 1 > Create variables for: Name, Age, Salary, Is Employee and print the value and its data type using type().")
+
+# name = "Ramesh"
+# age = 28
+# salary = 50000.50
+# is_employee = True
+
+# print(f"Name: {name}, Type: {type(name)}")
+# print(f"Age: {age}, Type: {type(age)}")
+# print(f"Salary: {salary}, Type: {type(salary)}")
+# print(f"Is Employee: {is_employee}, Type: {type(is_employee)}")
+
+# print("Exercise 2 > x = 10, print(type(x)), y = 10.5, print(type(y)) predict the output")
+# x = 10
+# print(f"type(x): {x}, Type:{type(x)}")
+# y = 10.5
+# print(f"type(y):{y}, Type: {type(y)}")
+
+print(f"Exercise 3 > Convert the following string to an integer. test_case = \"250\" ")
+test_case = "250"
+print(f"Before conversion: test_case = {test_case}, Type: {type(test_case)}")
+test_case = int(test_case)
+print(f"After conversion: test_case = {test_case}, Type: {type(test_case)}")
